a7f0aa1c0fa8b48e7ff08a689a03176629b5453b
core2drew <calupesandro@gmail.com>
2024-05-02 20:18:58 +0800

    chore: Integrate hook to fetch data and map it to the application - Display spinner when it is loading

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,21 @@ import Vehicle from "./components/Vehicle";
 // TODO
 // Load the mockedVehicles from the GraphQL API
 // See ./mockedVehicles.ts for API details
-import { mockedVehicles } from "./mockedVehicles";
+// import { mockedVehicles } from "./mockedVehicles";
+import { useQueryVehicles } from "./hooks/useQueryVehicles";
+import { Spinner } from "./components/Spinner";
 
 function App() {
+  const { data: vehicles, loading } = useQueryVehicles();
+
   return (
     <div className={styles.app}>
+      {loading && <Spinner />}
       <img src={vyroSquareLogo} className={styles.logo} alt="Vyro logo" />
       <h1>Welcome to Vyro</h1>
       <div className={styles.grid}>
-        {mockedVehicles.map((vehicle) => (
-          <Vehicle {...vehicle} />
+        {vehicles.map((vehicle, index) => (
+          <Vehicle key={index} {...vehicle} />
         ))}
       </div>
     </div>
